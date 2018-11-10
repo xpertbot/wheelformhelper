3e57e6d3da6aabbb1127169ec7284f693163c1e3
Roger <orantesrd@gmail.com>
2018-11-10 13:36:59 -0600

    Better examples and more comments

--- README.md
+++ README.md
@@ -5,11 +5,14 @@ In order to install this plugin in your environtment.
 Download these files and put them on a `plugins` folder next to your `vendor` folder.
 
 Add next JSON Object to the repositories array on your main `composer.json` for your craft install.
-(Note: you might need to adjust if "repositories" array if is not empty)
+(Note: you might need to adjust if "repositories" array is not empty)
 
 ```JSON
 {
-    "repositories": [
+  "require": {
+    "xpertbot/wheelformhelper": "*"
+  },
+  "repositories": [
     {
       "type": "path",
       "url": "plugins/wheelformhelper"
@@ -23,7 +26,7 @@ from your `composer.json` file.
 Note: the package name `xpertbot` and the plugin name `wheelformhelper` are irrelevant, you can change them to meet your needs.
 Make sure you update the `composer.json` inside the plugin folder and change the `name` attribute to the one you chose.
 
-Once the Path of the plugin is added to your `composer.json` you can run `composer require xpertbot\wheelformhelper`
+Once the Path of the plugin is added to your `composer.json` you can run `composer update xpertbot\wheelformhelper`
 (or whatever `package\name` you chose)
 
 [More Information](https://docs.craftcms.com/v3/plugin-intro.html#getting-started)
--- src/Plugin.php
+++ src/Plugin.php
@@ -13,11 +13,40 @@ class Plugin extends \craft\base\Plugin
 
         Event::on(Mailer::class, Mailer::EVENT_BEFORE_SEND, function($event)
         {
+            //Add extra fields to message
             $event->message['extra_field'] = [
                 'label' => 'New Label',
                 'value' => 'Lorem Ipsum',
                 'type' => "text"
             ];
+
+            //Add static Reply_to email
+            $event->reply_to = 'reply@example.com';
+
+            //Add reply_to based on form field
+            if($event->form_id == 1) {
+                if(! empty($event->message['email']['value'])) {
+                    $event->reply_to = $event->message['email']['value'];
+                }
+            }
+
+            //Conditional To Emails
+            //If user selected Chocolate as favorite 'flavour' field. (this can be radio, select, text, etc)
+            //It's a good idea to add more checks such as form ID and valid email.
+            if(array_key_exists('flavour', $event->message)) {
+                if($event->message['flavour']['value'] == 'Chocolate' ) {
+                    $event->to[] = 'chocolate@example.com';
+                }
+            }
+
+            //If user selected specific 'toppings' in a checkbox field.
+            if (array_key_exists('toppings', $event->message)) {
+                if (is_array($event->message['toppings'])) {
+                    if(in_array('Fudge', $event->message['toppings']['value'])) {
+                        $event->to[] = 'fudge@example.com';
+                    }
+                }
+            }
         });
     }
 }
